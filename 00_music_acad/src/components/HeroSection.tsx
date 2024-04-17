f4353c22ede11889bed408f59c30a75b60c5c410
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-04-17 22:33:24 +0530

    create hero section

--- 00_music_acad/src/components/HeroSection.tsx
+++ 00_music_acad/src/components/HeroSection.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
 
 function HeroSection() {
     return (
@@ -10,12 +13,15 @@ function HeroSection() {
                     className="-top-40 left-0 md:left-60 md:-top-20"
                     fill="white"
                 />
-                <h1>Master the art of music</h1>
-                <p> Dive into............</p>
+                <h1 className="text-3xl md:text-5xl md:leading-tight font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-800 mt-20">Enlighten Your Lens: Master Light Photography</h1>
+                <p className="text-xl mt-4 font-normal md:text-lg text-neutral-300 max-w-lg mx-auto">Unveil the secrets of light and shadow in our captivating photography course, where every click ignites a world of visual storytelling. Join us to capture moments that shimmer with brilliance, and let your imagination dance in the luminous artistry of photography.</p>
                 <div className="mt-4">
-                    <Link href={"/courses"}>
-                        Explore Courses
-                    </Link>
+                    <Button
+                        borderRadius="1.75rem"
+                        className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                        Explore
+                    </Button>
                 </div>
             </div>
         </div>
