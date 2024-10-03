c33be33f6e31e524be372d465ae6187437ac55a1
Bhaskar Lal Das <120587599+MrBLD@users.noreply.github.com>
2024-10-03 21:02:48 +0530

    update

--- 01_mystry_message/src/schemas/acceptMessageSchema.ts
+++
@@ -1,8 +0,0 @@
-import {boolean, z} from 'zod'
-
-export const acceptMessageSchema=z.object({
-    acceptMessage: z.boolean(),
-    
-}) 
-
-export type acceptMessageSchema=z.infer<typeof acceptMessageSchema>
